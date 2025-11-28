1bd211cbc4dd079b2a6dbb0e9936f4e1bb738e7c
Cauã Motta <ocauamotta@gmail.com>
2025-11-28 09:25:34 -0300

    feat: create repository class and auth service class

---
+++ PetLar_BackEnd/src/main/java/br/com/ocauamotta/PetLar/repositories/IUserRepository.java
@@ -0,0 +1,30 @@
+package br.com.ocauamotta.PetLar.repositories;
+
+import br.com.ocauamotta.PetLar.models.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Interface de Repositório para a entidade {@code User}.
+ * Estende {@code MongoRepository} para fornecer operações CRUD, paginação e
+ * consultas personalizadas no banco de dados MongoDB.
+ * <p>
+ * O tipo de entidade gerenciada é {@code User}
+ * e o tipo da chave primária (ID) é {@code String}.
+ */
+@Repository
+public interface IUserRepository extends MongoRepository<User, String> {
+    /**
+     * Busca um usuário pelo seu nome de usuário (username).
+     *
+     * <p>Este método é essencial para o processo de autenticação do Spring Security,
+     * pois retorna um objeto {@code UserDetails}
+     * que contém as informações necessárias para verificar as credenciais.
+     *
+     * @param username O nome de usuário a ser buscado.
+     * @return Um objeto {@code UserDetails}
+     * que representa o usuário encontrado, ou {@code null} se o usuário não existir.
+     */
+    UserDetails findByUsername(String username);
+}
---
+++ PetLar_BackEnd/src/main/java/br/com/ocauamotta/PetLar/services/AuthService.java
@@ -0,0 +1,38 @@
+package br.com.ocauamotta.PetLar.services;
+
+import br.com.ocauamotta.PetLar.repositories.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * Serviço de Autenticação responsável por carregar os detalhes de um usuário
+ * durante o processo de autenticação do Spring Security.
+ * <p>
+ * Esta classe implementa a interface {@code UserDetailsService},
+ * sendo obrigatória para a integração com o mecanismo de login do framework.
+ */
+@Service
+public class AuthService implements UserDetailsService {
+
+    @Autowired
+    private IUserRepository repository;
+
+    /**
+     * Carrega os detalhes do usuário a partir do seu username.
+     * <p>
+     * Este é o método principal invocado pelo Spring Security durante uma tentativa de login.
+     * Ele delega a busca ao repositório {@code IUserRepository}.
+     *
+     * @param username O nome de usuário fornecido na tentativa de autenticação.
+     * @return Um objeto {@code UserDetails}
+     * contendo os detalhes do usuário para verificação de senha e autorizações.
+     * @throws UsernameNotFoundException Se o usuário com o username especificado não for encontrado.
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByUsername(username);
+    }
+}
